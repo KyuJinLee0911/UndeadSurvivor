cb9c02faa387208c2098264f71dddd3d6e48a6d1
KyuJinLee0911 <rbwls1001@naver.com>
2023-05-30 16:08:29 +0900

    2023.05.30
    - 프로젝트 생성
    - 플레이어 생성 및 InputSystem을 이용한 플레이어 조작
    - 타일맵과 콜라이더를 통해 맵이 계속해서 이어질 수 있도록 하는 기능 추가
    - Cinemachine을 통해 카메라가 플레이어를 따라다니도록 함

---
+++ Undead Survivor/Assets/script/Player.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float speed = 8;
+    private Rigidbody2D playerRB;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate() 
+    {
+        Vector2 _nextVec = inputVec * speed * Time.fixedDeltaTime;
+        playerRB.MovePosition(playerRB.position + _nextVec);
+    }
+
+    private void LateUpdate() 
+    {
+        if(inputVec.x != 0)
+        {   
+            spriteRenderer.flipX = (inputVec.x < 0);
+        }
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+        animator.SetBool("isMove", inputVec.magnitude > 0.01);
+    }
+}
